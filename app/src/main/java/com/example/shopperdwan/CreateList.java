1d420f94d546eea15c72ead6c9905065997bc3a9
Dwan Conyers <Dwan.conyers@gmail.com>
2021-01-28 23:43:16 -0500

    Thursday 28 2021

---
+++ app/src/main/java/com/example/shopperdwan/CreateList.java
@@ -0,0 +1,23 @@
+package com.example.shopperdwan;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class CreateList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+}
