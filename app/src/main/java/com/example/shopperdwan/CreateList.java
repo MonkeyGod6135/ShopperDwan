b397122af871fd872b25e5b10ad8976b62fc91e8
Dwan Conyers <Dwan.conyers@gmail.com>
2021-02-02 10:46:29 -0500

    Tuesday Feb 2 2021

--- app/src/main/java/com/example/shopperdwan/CreateList.java
+++ app/src/main/java/com/example/shopperdwan/CreateList.java
@@ -1,16 +1,30 @@
 package com.example.shopperdwan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import java.util.Calendar;
+
 
 public class CreateList extends AppCompatActivity {
+
+    Intent intent;
+
+    //declare edittexts
+    EditText nameEditText;
+    EditText storeEditText;
+    EditText dateEditText;
+
+    //declare calender
+    Calendar calendar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +33,46 @@ public class CreateList extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        nameEditText = (EditText) findViewById(R.id.nameEditText);
+        storeEditText = (EditText) findViewById(R.id.storeEditText);
+        dateEditText = (EditText) findViewById(R.id.dateEditText);
+
     }
+
+    /**
+     * This method further intializes the action bar activity
+     * @param menu menu resource file for the activity
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_create_list, menu);
+        return true;
+    }
+
+    /**
+     * This method is called when a menu item in a overflow menu is selected
+     * and it controls what happens when it is selected
+     * @param item selected item in overflow menu
+     * @return true if menu item is selected, else false
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_home:
+                intent = new Intent(this, CreateList.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_create_list:
+                intent = new Intent(this, CreateList.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/shopperdwan/MainActivity.java
+++ app/src/main/java/com/example/shopperdwan/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.shopperdwan;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -15,6 +14,12 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent intent;
+
+    /**
+     * This method intializies the action bar and view of the activity
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * This method further intializes the action bar activity
+     * @param menu menu resource file for the activity
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -32,19 +42,28 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * This method is called when a menu item in a overflow menu is selected
+     * and it controls what happens when it is selected
+     * @param item selected item in overflow menu
+     * @return true if menu item is selected, else false
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()){
+            case R.id.action_home:
+                intent = new Intent(this, CreateList.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_create_list:
+                intent = new Intent(this, CreateList.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void openCreateList(View view) {
