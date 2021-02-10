cc6b503e93447b1b693aff0d6bb7b32986cabe46
Dwan Conyers <Dwan.conyers@gmail.com>
2021-02-09 23:06:13 -0500

    Tuesday feb 9th 2021

--- app/src/main/java/com/example/shopperdwan/CreateList.java
+++ app/src/main/java/com/example/shopperdwan/CreateList.java
@@ -31,6 +31,8 @@ public class CreateList extends AppCompatActivity {
     //declare calender
     Calendar calendar;
 
+    DBHandler dbHandler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,7 +82,7 @@ public class CreateList extends AppCompatActivity {
                         date,
                         calendar.get(Calendar.YEAR),
                         calendar.get(Calendar.MONTH),
-                        calendar.get(Calendar.DAY_OF_MONTH));
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
 
             }
         });
@@ -139,6 +141,8 @@ public class CreateList extends AppCompatActivity {
             Toast.makeText(this, "Please enter name, store, and date!",
                     Toast.LENGTH_LONG).show();
         } else{
+            dbHandler.addShoppingList(name, store, date);
+
             Toast.makeText(this, "Shopping List Created!",
                     Toast.LENGTH_LONG).show();
 
--- app/src/main/java/com/example/shopperdwan/DBHandler.java
+++ app/src/main/java/com/example/shopperdwan/DBHandler.java
@@ -1,6 +1,8 @@
 package com.example.shopperdwan;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -35,17 +37,76 @@ public class DBHandler extends SQLiteOpenHelper {
      */
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        //define create statement for shopping list and store it in a string
+        String query = "CREATE TABLE " + TABLE_SHOPPING_LIST + "( " +
+                COLUMN_LIST_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_LIST_NAME+ "text, " +
+                COLUMN_LIST_STORE+ "text, " +
+                COLUMN_LIST_DATE+ "text; ";
+
+        //execute the statement
+        sqLiteDatabase.execSQL(query);
+
 
     }
 
     /**
      * Creates a new version
-     * @param db
+     * @param sqLiteDatabase
      * @param oldVersion
      * @param newVersion
      */
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+        //define drop statement and store it in a string
+        String query = "DROP TABLE IF EXISTS " + TABLE_SHOPPING_LIST;
+
+        //execute statement
+        sqLiteDatabase.execSQL(query);
+
+        onCreate(sqLiteDatabase);
 
     }
+
+    /**
+     * This method gets called whhen the add button in the action bar of the createlist activity
+     * gets clicked and makes a new row into the shopping list table
+     * @param name Shopping list name
+     * @param store Shopping list store
+     * @param date Shopping list date
+     */
+    public void addShoppingList(String name, String store, String date){
+        //refernce to database
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        //put data into content values
+        values.put(COLUMN_LIST_NAME, name);
+        values.put(COLUMN_LIST_STORE, store);
+        values.put(COLUMN_LIST_DATE, date);
+
+        //insert data in Content values into shopping list table
+        db.insert(TABLE_SHOPPING_LIST, null, values);
+
+        db.close();
+
+
+
+    }
+
+    public Cursor getShoppingList(){
+        //reference to database
+        SQLiteDatabase db = getWritableDatabase();
+
+        //define statement and store it in string
+        String query ="SELECT + FROM " + TABLE_SHOPPING_LIST;
+
+        //execute statement
+        return db.rawQuery(query, null);
+
+
+    }
+
 }
